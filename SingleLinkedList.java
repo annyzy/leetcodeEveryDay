4a6e34a3672745a1d7176c58082f0b883caab1e9
annyzy <ann@newloong.com>
2021-08-23 15:59:32 -0700

    add new function addTwoNumbers w/o test cases yet

--- SingleLinkedList.java
+++ SingleLinkedList.java
@@ -205,6 +205,44 @@ public class SingleLinkedList {
 		prev.next = next;
 	}
 
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 ==  null && l2 == null ){
+            System.out.println("Invalid two lists provided.");
+            return null;
+        }  
+        
+        ListNode dummy = new ListNode(0);
+        ListNode a = l1, b = l2, curr = dummy;
+        int carry = 0;
+        
+        while( a!= null || b!= null){
+            int val1, val2;
+            if( a != null){
+                val1 = a.val;
+                a = a.next;
+            }else{
+                val1 = 0; 
+            }
+            
+            if( b != null){
+                val2 = b.val;
+                b=b.next;
+            }else{
+                val2 = 0;
+            }
+            
+            int sum = val1+val2+carry;
+            carry = sum/10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+                    
+        if(carry > 0){
+            curr.next = new ListNode(carry);
+        } 
+        return dummy.next; 
+    }
+
 	public static void main(String[] args) {
 		SingleLinkedList l1 = new SingleLinkedList();
 		l1.appendToTail(1);
