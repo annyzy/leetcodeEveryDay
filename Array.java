b1c9a3da237b6a9158d238234d7385a8455fc8d8
annyzy <ann@newloong.com>
2021-08-28 13:05:37 -0700

    add new function mergeSortedArray

--- Array.java
+++ Array.java
@@ -57,4 +57,11 @@ class Array {
         }
         return triangle;
     }
+
+    public void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < n; i ++){
+            nums1[m+i] = nums2[i];
+        }   
+        Arrays.sort(nums1);
+    }
 }
