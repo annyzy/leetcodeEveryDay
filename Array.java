c4bc6e585a8cb876bbb7da929876513abfeb50da
annyzy <ann@newloong.com>
2021-08-29 11:52:18 -0700

    add new function countPrimes

--- Array.java
+++ Array.java
@@ -143,4 +143,28 @@ class Array {
         }
         return false;
     }
+
+    public int countPrimes(int n) {
+        if (n < 2)
+            return 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+
+        // numbers smaller than i*i might be checked before
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i])
+                count++;
+        }
+
+        return count;
+    }
 }
