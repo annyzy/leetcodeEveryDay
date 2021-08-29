15c4d4051f166c4f878b2e15c150679bfcf08f25
annyzy <ann@newloong.com>
2021-08-29 13:56:55 -0700

    add new function intersectOfTwoArrays

--- Array.java
+++ Array.java
@@ -7,8 +7,10 @@ class Array {
         int prev = 0, ans = nums[0];
 
         for (int x : nums) {
-            // if the previous sum is smaller or equal x, trash the previous sum and restart
-            // again from x
+            /*
+             * if the previous sum is smaller or equal x, trash the previous sum and restart
+             * again from x
+             */
             prev = Math.max(prev + x, x);
             // find max subarray
             ans = Math.max(ans, prev);
@@ -17,7 +19,7 @@ class Array {
     }
 
     public int[] plusOne(int[] digits) {
-        if (nums == null || nums.length == 0)
+        if (digits == null || digits.length == 0)
             return null;
         int len = digits.length;
         for (int i = len - 1; i >= 0; i--) {
@@ -102,8 +104,10 @@ class Array {
 
         HashSet<Integer> set = new HashSet<Integer>();
         for (int i = 0; i < nums.length; i++) {
-            // return true is the element is not already present in the set, false if the
-            // set already contains ans remain unchange
+            /*
+             * return true is the element is not already present in the set, false if the
+             * set already contains ans remain unchange
+             */
             if (!set.add(nums[i])) {
                 set.remove(nums[i]);
             }
@@ -135,8 +139,10 @@ class Array {
 
         HashSet<Integer> set = new HashSet<Integer>();
         for (int i = 0; i < nums.length; i++) {
-            // return true is the element is not already present in the set, false if the
-            // set already contains ans remain unchange
+            /*
+             * return true is the element is not already present in the set, false if the
+             * set already contains ans remain unchange
+             */
             if (!set.add(nums[i]))
                 return true;
             set.add(nums[i]);
@@ -166,5 +172,65 @@ class Array {
         }
 
         return count;
+    }
+
+    public int[] intersectOfTwoArrays(int[] nums1, int[] nums2) {
+        // edge cases
+        if (nums1 == null || nums1.length == 0)
+            return null;
+        if (nums2 == null || nums2.length == 0)
+            return null;
+        if (nums1.length == 0 && nums2.length == 0)
+            return null;
+        // iterate the shorter array first to reduce the space complexity
+        if (nums1.length > nums2.length)
+            return intersect(nums2, nums1);
+
+        /*
+         * Creat a map to store the elements in nums1 as key, and the numbers of each
+         * elements appears as value
+         */
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int x : nums1) {
+            /*
+             * count the value if a key is mapped, start from 0 if map contains no mapping
+             * key
+             */
+            int count = map.getOrDefault(x, 0) + 1;
+            map.put(x, count);
+        }
+
+        int[] ans = new int[nums1.length];
+        int i = 0;
+        for (int x : nums2) {
+            /*
+             * count the value if nums2'x key is mapped in nums1's, start from 0 if map
+             * contains no mapping key
+             */
+            int count = map.getOrDefault(x, 0);
+            // there is an intersection between two arrays
+            if (count > 0) {
+                ans[i] = x;
+                i++;
+                // remove that intersection
+                count--;
+                /*
+                 * if an intersected nums has more than one update it's count after removing
+                 * once
+                 */
+                if (count > 0) {
+                    map.put(x, count);
+                } else {
+                    // only intersection once, then remove from the map after updating
+                    map.remove(x);
+                }
+            }
+        }
+        /*
+         * only get the range we need, otherwise there is a 0 in the end copy the range
+         * from 0 to i of ans to a new array
+         */
+        return Arrays.copyOfRange(ans, 0, i);
     }
 }
