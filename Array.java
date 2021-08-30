4842d65c30e1bc5303683a02709bec642ba9ba6e
annyzy <ann@newloong.com>
2021-08-29 21:18:54 -0700

    add new function isAnagram1

--- Array.java
+++ Array.java
@@ -283,4 +283,18 @@ class Array {
         }
         return ans;
     }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.isEmpty() && t.isEmpty())
+            return false;
+        if (s.length() != t.length())
+            return false;
+        char s1[] = s.toCharArray();
+        char t1[] = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        return Arrays.equals(s1, t1);
+    }
 }
