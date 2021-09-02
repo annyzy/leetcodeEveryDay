c70351948fb2a7c9bf486fd125a7616dfac7bd82
annyzy <ann@newloong.com>
2021-09-01 20:22:11 -0700

    update longestPalindrome

--- dynamicProgramming.java
+++ dynamicProgramming.java
@@ -25,17 +25,26 @@ class dynamicProgramming {
         boolean[][] dp = new boolean[len][len];
         String ans = "";
 
+        /*    j 0 1 2 3
+            i   a a b a
+            0 a T T F F
+            1 a F T F T
+            2 b F F T F
+            3 a F F F T
+        */
         for(int  j = 1; j < len; j++){
             for (int i = 0; i < j; i++){
                 if(s.charAt(i) != s.charAt(j)) continue;
                 if(i == j) {
                     dp[i][j] = true;
                 } else if(j-i <=2){
+                    //"aa". "aba"
                     dp[i][j] = true;
                 }else{
+                    //"axxxxxa", only need to check if xxxxx is palidrome of not
                     dp[i][j] = dp[i+1][j-1];
                 }
-
+                //get the largest palidrome if ans is palidrome
                 if(dp[i][j] && j - i +1 > ans.length()){
                     ans = s.substring(i, j+1);
                 }
