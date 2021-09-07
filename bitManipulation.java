77ffa81da43aa41ee8c98f5dfa6ac33332fdcc79
annyzy <ann@newloong.com>
2021-09-06 21:17:07 -0700

    add new function isHappy

--- bitManipulation.java
+++ bitManipulation.java
@@ -119,4 +119,22 @@ class bitManipulation {
         
         return ans;
     }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            int totalSum = 0;
+            while (n > 0) {
+                //get digits
+                int d = n % 10;
+                //get tens
+                n = n / 10;
+                totalSum += d * d;
+            }
+            n = totalSum;
+        }
+        //true if n==1; otherwise, false
+        return n==1;
+    }
 }
