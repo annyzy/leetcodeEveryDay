89f03eacb9c7877007f66c922f5849781b284276
annyzy <ann@newloong.com>
2021-09-06 21:21:48 -0700

    add new function isPowerOfThree

--- bitManipulation.java
+++ bitManipulation.java
@@ -137,4 +137,10 @@ class bitManipulation {
         //true if n==1; otherwise, false
         return n==1;
     }
+
+    public boolean isPowerOfThree(int n) {
+        if( n<=0 ) return false;
+        if(  (Math.log10(n) / Math.log10(3)) % 1 == 0) return true;
+        return false;
+    }
 }
