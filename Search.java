e0f6bde372a7b9187b364029e81adcf63537c817
annyzy <ann@newloong.com>
2021-10-03 12:02:25 -0700

    update function SearchinRotatedSortedArray using binary search method

--- Search.java
+++ Search.java
@@ -1,22 +1,42 @@
 import java.util.*;
 
 class Search {
+    // T: O(log n)
     public int SearchinRotatedSortedArray(int[] nums, int target) {
         int len = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // edge case:
-        if (len < 1)
+        if (len == 0)
             return -1;
+        return binarysearch(nums, 0, len - 1, target);
+    }
 
-        for (int i = 0; i < len; i++) {
-            map.put(nums[i], i);
-        }
+    public int binarysearch(int[] nums, int l, int r, int target) {
 
-        if (!map.containsKey(target)) {
-            return -1;
-        } else {
-            return map.get(target);
+        while (l <= r) {
+            // get the middle position
+            int m = l + (r - l) / 2;
+            if (nums[m] == target)
+                return m;
+
+            // if [l,m] is ascending sorted order
+            if (nums[l] <= nums[m]) {
+                // search from [l,m-1]
+                if (target >= nums[l] && target < nums[m]) {
+                    r = m - 1;
+                    // search from [m+1,l]
+                } else {
+                    l = m + 1;
+                }
+                // if (m,r] is ascending sorted order
+            } else {
+                // search from [m+1,r]
+                if (target > nums[m] && target <= nums[r]) {
+                    l = m + 1;
+                    // search from [l,m-1]
+                } else {
+                    r = m - 1;
+                }
+            }
         }
+        return -1;
     }
 }
