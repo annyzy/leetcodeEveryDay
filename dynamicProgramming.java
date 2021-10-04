463283d643d942f0c00fd7021891866eec0b8c41
annyzy <ann@newloong.com>
2021-10-03 21:50:46 -0700

    add new function wordBreak

--- dynamicProgramming.java
+++ dynamicProgramming.java
@@ -86,4 +86,26 @@ class dynamicProgramming {
         }
         return ans;        
     }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        //edge case
+        if(s.length() <= 0 ) return false;
+        
+        int len = s.length();
+        Set<String> set = new HashSet<String>(wordDict);
+        boolean[] dp = new boolean[len + 1];
+        
+        dp[0] = true;
+        
+        for(int i  = 1; i <= len; i ++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[len];
+    }
 }
