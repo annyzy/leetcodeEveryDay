72c3f30e9deb663b62ff66ca67d5338e92161fa6
annyzy <ann@newloong.com>
2021-10-15 21:58:43 -0700

    create a series of questions solved by hashset and add new function longestConsecutive

---
+++ Hashset.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Hashset {
+    public int longestConsecutive(int[] nums) {
+        //edge case
+        if(nums.length == 0) return 0;
+        
+        Set<Integer> set =new HashSet<>();
+        
+        for(int num: nums)
+            set.add(num);
+        
+        int longest = 0;
+        
+        for(int num: set){
+            if( !set.contains(num-1)){
+                int curr = num;
+                int currLen = 1;
+                
+                while(set.contains(curr + 1)){
+                    currLen +=1;
+                    curr+=1;
+                }
+                
+                longest = Math.max(currLen,longest);
+            }
+        }
+        
+        return longest;
+    }
+}
