4fd39570e9fd39e43f6fa57003599c9b61cf5c95
annyzy <ann@newloong.com>
2021-10-21 18:09:50 -0700

    add new function uniquePathsWithObstacles

--- Zilou.java
+++ Zilou.java
@@ -33,4 +33,39 @@ class Zilou {
         }
         return dp[n];
     }
+
+    // Time: O(mn)
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+
+        if (m < 1 || n < 1)
+            return 0;
+
+        int[][] dp = new int[m][n];
+
+        // initialize the first column
+        for (int i = 0; i < m; i++) {
+            // once meet the obstacle, not going to the rest of the grids
+            if (obstacleGrid[i][0] == 1)
+                break;
+            // mark as 1 as there is one way to reach as the first step
+            dp[i][0] = 1;
+        }
+
+        // same initalize the first row
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1)
+                break;
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] != 1)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
 }
