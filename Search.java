144425acbc778328503eb0ef2eb3c54ccb9c4869
annyzy <ann@newloong.com>
2021-11-15 19:58:00 -0800

    lc34

--- Search.java
+++ Search.java
@@ -39,4 +39,53 @@ class Search {
         }
         return -1;
     }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = new int[] { -1, -1 };
+        // edge case
+        if (nums == null || nums.length == 0)
+            return ans;
+
+        ans[0] = searchLeftMost(nums, target);
+        ans[1] = searchRightMost(nums, target);
+
+        return ans;
+    }
+
+    // T: O(log n)
+    public int searchLeftMost(int[] nums, int target) {
+        int ans = -1;
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                ans = mid;
+            if (nums[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int searchRightMost(int[] nums, int target) {
+        int ans = -1;
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                ans = mid;
+            if (nums[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
+    }
 }
