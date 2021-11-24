80a96d18ad0897efc71dfb8b3a5d6a48631f86da
annyzy <ann@newloong.com>
2021-11-24 06:32:58 -0800

    lc35-binarysearch

--- Search.java
+++ Search.java
@@ -85,7 +85,20 @@ class Search {
                 r = mid - 1;
             }
         }
-
         return ans;
+    }
+
+    public int searchInsert(int[] nums, int target){
+        int l  = 0, r = nums.length - 1;
+        while (l <= r){
+            int mid = l + (r-l)/2;
+            if( nums[mid] == target) return mid;
+            if( target < nums[mid]){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 }
