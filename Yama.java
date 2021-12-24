1bb8282d0ec06427e60af8da9a800fad940a93f1
annyzy <ann@newloong.com>
2021-12-24 15:49:40 -0800

    update

--- Yama.java
+++ Yama.java
@@ -23,4 +23,27 @@ class Yama {
         }
         return -1;
     }
+
+    public static int groupDivision(int[] levels, int maxSpread) {
+        int count = 1;
+        Arrays.sort(levels);
+
+        int curr = levels[0];
+        int i = 0;
+        for (int level : levels) {
+            if (level > curr + maxSpread) {
+                ++count;
+                ++i;
+                curr = levels[i];
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] lists = { 1, 4, 7, 3, 4 };
+        int maxSpread = 2;
+        groupDivision(lists, maxSpread);
+    }
 }
