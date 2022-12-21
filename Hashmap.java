b8c35f823abe53365df4edde3dad4cc7bc2fc5df
ziyingy <41768592+annyzy@users.noreply.github.com>
2022-12-20 22:39:51 -0800

    Add Easy: twoSum

--- Hashmap.java
+++ Hashmap.java
@@ -1,6 +1,18 @@
 import java.util.*;
 
 class Hashmap {
+    //Easy: Two Sum
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i){
+            if ( map.containsKey(target - nums[i]) ){
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+    
     public int[] topKFrequent(int[] nums, int k) {
         //edge case
         if(nums.length < 1 || k < 1) return null;
